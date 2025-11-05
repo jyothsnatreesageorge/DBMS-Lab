0f26b9a1659e64aa4a5506df4c5702ed1e1f9ed6
Jyothsna Treesa George <168282797+jyothsnatreesageorge@users.noreply.github.com>
2025-11-05 09:18:24 +0530

    Update Experiment 2.sql

--- CURSOR/Experiment 2.sql
+++ CURSOR/Experiment 2.sql
@@ -27,14 +27,14 @@ SQL> DECLARE
       v_rel Employee.relieving_date%TYPE;
       v_years NUMBER;
     BEGIN
-    OPEN emp_cur;
-    LOOP
-      FETCH emp_cur INTO v_id, v_name, v_join, v_rel;
-      EXIT WHEN emp_cur%NOTFOUND;
-      v_years := TRUNC(MONTHS_BETWEEN(v_rel, v_join) / 12);
-      DBMS_OUTPUT.PUT_LINE('EmpID: ' || v_id || ' Name: ' || v_name || ' Service: ' || v_years || ' years');
-    END LOOP;
-    CLOSE emp_cur;
+          OPEN emp_cur;
+          LOOP
+            FETCH emp_cur INTO v_id, v_name, v_join, v_rel;
+            EXIT WHEN emp_cur%NOTFOUND;
+            v_years := TRUNC(MONTHS_BETWEEN(v_rel, v_join) / 12);
+            DBMS_OUTPUT.PUT_LINE('EmpID: ' || v_id || ' Name: ' || v_name || ' Service: ' || v_years || ' years');
+          END LOOP;
+          CLOSE emp_cur;
     END;
     /
 
@@ -42,3 +42,31 @@ SQL> DECLARE
 OUTPUT
 EmpID: 101 Name: Anita Service: 10 years
 EmpID: 102 Name: Binu Service: 6 years
+
+
+SQL> DECLARE
+      CURSOR pension_cur IS SELECT empid, empname, joining_date, relieving_date, salary FROM Employee WHERE relieving_date IS NOT NULL;
+      v_id Employee.empid%TYPE;
+      v_name Employee.empname%TYPE;
+      v_join Employee.joining_date%TYPE;
+      v_rel Employee.relieving_date%TYPE;
+      v_sal Employee.salary%TYPE;
+      v_years NUMBER;
+      v_pension NUMBER;
+      BEGIN
+            OPEN pension_cur;
+            LOOP
+                  FETCH pension_cur INTO v_id, v_name, v_join, v_rel, v_sal;
+                  EXIT WHEN pension_cur%NOTFOUND;
+                  v_years := TRUNC(MONTHS_BETWEEN(v_rel, v_join) / 12);
+                  v_pension := (v_years * v_sal) / 100;
+                  DBMS_OUTPUT.PUT_LINE('EmpID: ' || v_id || ' Name: ' || v_name || ' Pension: ' || v_pension);
+            END LOOP;
+            CLOSE pension_cur;
+      END;
+      /
+
+
+OUTPUT
+EmpID: 101 Name: Anita Pension: 4000
+EmpID: 102 Name: Binu Pension: 2100
